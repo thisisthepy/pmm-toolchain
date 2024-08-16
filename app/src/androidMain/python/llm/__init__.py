7b9aba8ca73a899c15ec8dcfeba49460012f0bf7
BREW <IRACK000@gmail.com>
2024-08-17 02:03:30 +0900

    Refactor: Path style

--- app/src/androidMain/python/llm/__init__.py
+++ app/src/androidMain/python/llm/__init__.py
@@ -6,9 +6,9 @@ llama_cpp_lib_path = os.path.join(assets_path, "requirements", "llama_cpp", "lib
 
 
 import numpy  # Or any requirement other than llama_cpp
-numpy.__loader__.finder.extract_if_changed("llama_cpp/lib/libllama.so")
-numpy.__loader__.finder.extract_if_changed("llama_cpp/lib/libggml.so")
-numpy.__loader__.finder.extract_if_changed("llama_cpp/lib/libllava.so")
+numpy.__loader__.finder.extract_if_changed(os.path.join("llama_cpp", "lib", "libllama.so"))
+numpy.__loader__.finder.extract_if_changed(os.path.join("llama_cpp", "lib", "libggml.so"))
+numpy.__loader__.finder.extract_if_changed(os.path.join("llama_cpp", "lib", "libllava.so"))
 
 ctypes.CDLL(os.path.join(llama_cpp_lib_path, "libggml.so"))
 
